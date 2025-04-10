3e7e4ca33247c8e4cf8861e5f6148cbcfcd64c1c
clarence <reyannpahinado@gmail.com>
2025-04-10 15:50:16 +0800

    Added Validation for forms

--- script.js
+++ script.js
@@ -2,11 +2,53 @@ const modal = document.getElementById("myModal");
 const openModalBtn = document.getElementById("openModalBtn");
 const closeModalSpan = document.querySelector(".close");
 const bookForm = document.getElementById("book-form");
-const submitForm = document.getElementById("submit-form");
 const booksContainer = document.getElementById("books-container");
 
 const booksCollection = [];
 
+bookForm.addEventListener("submit", (ev) => {
+  ev.preventDefault();
+  let allValidation = true;
+  let allData = {};
+  ev.target.querySelectorAll("input").forEach((element, index) => {
+    if (!validation(element)) allValidation = false;
+    if (allValidation) allData[index] = { [element.name]: element.value };
+  });
+
+  if (allValidation)
+    booksCollection.push(
+      new createBook(
+        allData[0].title.toUpperCase(),
+        allData[1].author,
+        allData[2].pages,
+        false
+      )
+    );
+  ev.target.reset();
+  renderBook();
+});
+
+Array.from(bookForm).forEach((element) => {
+  element.addEventListener("input", (event) => {
+    validation(event.target);
+  });
+});
+
+const validation = (data) => {
+  const currentInput = data;
+  if (!currentInput.checkValidity()) {
+    currentInput.nextElementSibling.textContent =
+      currentInput.validationMessage;
+    currentInput.classList.add("error-input");
+    return false;
+  } else {
+    currentInput.classList.remove("error-input");
+    currentInput.nextElementSibling.textContent = "";
+    return true;
+  }
+};
+
+//constructor for creating book contents
 class createBook {
   constructor(title, author, pages, status) {
     this._title = title;
@@ -30,66 +72,75 @@ class createBook {
   }
 }
 
-bookForm.addEventListener("submit", (ev) => {
-  ev.preventDefault();
-  const title = ev.target.title.value;
-  const author = ev.target.author.value;
-  const pages = ev.target.pages.value;
-  booksCollection.push(
-    new createBook(title.toUpperCase(), author, pages, false)
-  );
-  ev.target.reset();
+//handles rendering elements and event handlers
+class bookComponent {
+  constructor(bookDetails, removeBookHandler, bookId) {
+    this._bookDetails = bookDetails;
+    this._bookId = bookId;
+    this._removeBookHandler = removeBookHandler;
+  }
+  createElement(element, className, textContent = "") {
+    const ele = document.createElement(element);
+    if (className !== "") ele.classList = className;
+    if (textContent !== "") ele.textContent = textContent;
+    return ele;
+  }
+  renderElement() {
+    const bookDiv = this.createElement("div", "book-div", "");
 
-  renderBook();
-});
+    const coverTop = this.createElement("div", "cover-top");
+    const coverBottom = this.createElement("div", "cover-bottom");
+    const author = this.createElement(
+      "p",
+      "",
+      "By " + this._bookDetails.book.author
+    );
+    const title = this.createElement("h2", "", this._bookDetails.book.title);
+    const pagesEle = this.createElement(
+      "p",
+      "",
+      this._bookDetails.book.pages + "p"
+    );
+    const changeStatusEl = this.createElement(
+      "button",
+      this._bookDetails.book.status ? "completed" : "default",
+      this._bookDetails.book.status ? "Completed" : "Mark as Completed"
+    );
+    const removeEl = this.createElement("button", "", "Remove");
+    changeStatusEl.addEventListener("click", (ev) => {
+      this.changeStatus();
+    });
 
+    removeEl.addEventListener("click", (ev) => {
+      this._removeBookHandler(this._bookId);
+    });
+    coverTop.append(author, title, pagesEle);
+    coverBottom.append(changeStatusEl, removeEl);
+    bookDiv.append(coverTop, coverBottom);
+    return bookDiv;
+  }
+  changeStatus() {
+    this._bookDetails._status = !this._bookDetails._status;
+    console.log(this.renderElement());
+    this.renderElement();
+  }
+}
 const renderBook = () => {
   booksContainer.textContent = "";
   booksCollection.forEach((val, key) => {
-    const bookDiv = document.createElement("div");
-    bookDiv.classList = "book-div";
-    const coverTop = document.createElement("div");
-    const coverBottom = document.createElement("div");
-    coverTop.classList = "cover-top";
-    coverBottom.classList = "cover-bottom";
-    const title = document.createElement("h2");
-    const pagesEle = document.createElement("p");
-    const authorEle = document.createElement("p");
-    const changeStatusEl = document.createElement("button");
-    const removeEl = document.createElement("button");
-
-    removeEl.addEventListener("click", () => {
-      removeBook(key);
-    });
-    changeStatusEl.addEventListener("click", () => {
-      changeStatus(key);
-    });
-    title.textContent = val._title;
-    authorEle.textContent = `By ${val._author}`;
-    pagesEle.textContent = `${val._pages} Pages`;
-    removeEl.textContent = "Delete Book";
-    changeStatusEl.textContent = val._status
-      ? "Completed"
-      : "Mark as Completed";
-    changeStatusEl.classList = val._status ? "completed" : "default";
-    coverTop.append(authorEle, title, pagesEle);
-    coverBottom.append(changeStatusEl, removeEl);
-    bookDiv.append(coverTop, coverBottom);
-    booksContainer.appendChild(bookDiv);
+    const renderB = new bookComponent(val, removeBook, key);
+    booksContainer.appendChild(renderB.renderElement());
   });
 };
-const changeStatus = (id) => {
-  booksCollection[id]._status = !booksCollection[id]._status;
-
+const changeStatus = (bookDetails) => {
+  bookDetails._status = !bookDetails._status;
   renderBook();
 };
 const removeBook = (id) => {
   const confirmation = confirm("Are u sure?");
-  if (confirmation) booksCollection.splice(id);
+  if (confirmation) booksCollection.splice(id, 1);
   renderBook();
 };
-
-renderBook();
 
 openModalBtn.onclick = function () {
   modal.style.display = "block";
